e49d85f0d79dfd25277610247a2f156d0f37b5cd
Forget2remember <39069310+Forget2remember@users.noreply.github.com>
2021-03-09 00:15:42 -0500

    File containing main
    I neglected to add an optional extended description. . .

---
+++ SortingAlgorithms_Group5.java
@@ -0,0 +1,41 @@
+/******************************************************************************\
+ *  @authors Garrett Hedgecock, Sonia Akkiangadi, {more}                      *
+ *  @instructor Chafic Bou-Saba                                               *
+ *  @institution Guilford College                                             *
+ *  @course CTIS-310                                                          *
+ *                                                                            *
+ *  Sorting Algorithms                                                        *
+ *                                                                            *
+ *  @instructions Each sorting algorithm should contain:                      *
+ *  1.) 10 fields to insert the number to be sorted either manually or using  *
+ *      the "Generate Random Numbers" button.                                 *
+ *  2.) The number should be limited between 0 - 99.                          *
+ *  3.) 3 buttons:                                                            *
+ *      a.) "Generate Random Numbers"                                         *
+ *      b.) "Start Sort"                                                      *
+ *      c.) "Reset Numbers"                                                   *
+ *                                                                            *
+\******************************************************************************/
+
+package sortingalgorithms_group5;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class SortingAlgorithms_Group5 extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
